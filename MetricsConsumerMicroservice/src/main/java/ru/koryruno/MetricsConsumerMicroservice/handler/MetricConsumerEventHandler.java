2c6ab8292b37b0e8ca03f7bc6bf0ed8b030fdcce
koryruno <koryruno@yandex.ru>
2024-08-29 12:26:47 +0300

    add: consumer

---
+++ MetricsConsumerMicroservice/src/main/java/ru/koryruno/MetricsConsumerMicroservice/handler/MetricConsumerEventHandler.java
@@ -0,0 +1,18 @@
+package ru.koryruno.MetricsConsumerMicroservice.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@KafkaListener(topics = "${kafka.topic.metrics}")
+public class MetricConsumerEventHandler {
+
+    @KafkaHandler
+    public void handle(MetricProducerEvent metricProducerEvent) {
+        log.info("Received event: {}", metricProducerEvent.getName());
+    }
+
+}
