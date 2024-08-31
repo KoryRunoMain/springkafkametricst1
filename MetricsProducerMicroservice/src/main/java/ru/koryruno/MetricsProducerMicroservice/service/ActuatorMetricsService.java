1aff2f1c65dde4e418e9abaa70e86d3a13be98b2
koryruno <koryruno@yandex.ru>
2024-08-31 13:58:02 +0300

    dev: consumer v5.stable

--- MetricsProducerMicroservice/src/main/java/ru/koryruno/MetricsProducerMicroservice/service/ActuatorMetricsService.java
+++ MetricsProducerMicroservice/src/main/java/ru/koryruno/MetricsProducerMicroservice/service/ActuatorMetricsService.java
@@ -2,13 +2,17 @@ package ru.koryruno.MetricsProducerMicroservice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpMethod;
 import ru.koryruno.MetricsProducerMicroservice.model.dto.MetricProducerDto;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -16,33 +20,6 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ActuatorMetricsService {
 
-    private final RestTemplate restTemplate;
-    private final MetricProducerService metricsProducerService;
 
-    // TODO come back with question how much fixedRate is needed
-    @Scheduled(fixedRate = 5000)
-    public void collectAndSendMetrics() {
-        log.info("Scheduled task is running");
-        try {
-            ResponseEntity<Map> metrics = restTemplate
-                    .getForEntity("http://localhost:8081/actuator/metrics", Map.class);
-
-            metrics.getBody().forEach((name, value) -> {
-                MetricProducerDto metric = MetricProducerDto.builder()
-                        .name(name.toString())
-                        .value(value.toString())
-                        .timestamp(Instant.now().toString())
-                        .build();
-
-                restTemplate.postForEntity(
-                        "http://localhost:8081/metrics",
-                        metric,
-                        String.class
-                );
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
 }
