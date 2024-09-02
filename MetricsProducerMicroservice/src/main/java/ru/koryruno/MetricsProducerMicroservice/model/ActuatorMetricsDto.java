842a253d8bac71a378e5f3a7fc8a30b38837cd46
koryruno <koryruno@yandex.ru>
2024-09-02 19:37:10 +0300

    done: project 0.0.1-SnapShot

--- MetricsProducerMicroservice/src/main/java/ru/koryruno/MetricsProducerMicroservice/model/ActuatorMetricsDto.java
+++ MetricsProducerMicroservice/src/main/java/ru/koryruno/MetricsProducerMicroservice/model/ActuatorMetricsDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
  * Data Transfer Object (DTO) for storing and transferring metrics data collected from the actuator.
  * <p>
  * This class is used to encapsulate metric data retrieved from the actuator's endpoints.
- * The data typically includes the metric's name, its value, and the timestamp when the metric was collected.
+ * The data typically includes the metrics name, its value, and the timestamp when the metric was collected.
  * </p>
  */
 @Data
